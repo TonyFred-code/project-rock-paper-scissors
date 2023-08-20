6319ca30f1635500c6cda68fe11650e63c203242
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2023-08-20 04:54:40 +0100

    Create getComputerChoice()

---
+++ external.js
@@ -0,0 +1,13 @@
+// Get computer choice
+function getComputerChoice() {
+    const choices = ["Rock", "Paper", "Scissors"];
+    return choices[Math.floor(Math.random() * 3)];
+}
+// "Rock" or "Paper" or "Scissors"
+// Get player choice
+// Use prompts and make choice case//insensitive
+// Use player's and computer choices to play round
+// Inform user of winner of the round
+// Make game run through 5 rounds to determine winner
+// Inform user of winner of game after every 5 rounds
+// Give option to restart playing game
